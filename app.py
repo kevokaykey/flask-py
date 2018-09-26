09a8448e1bbd3767135aa1b726646d918ff5d08b
kevin <kkemboi08@gmail.com>
2018-09-26 17:45:14 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from data import Articles
+
+app = Flask(__name__)
+app.debug = True
+
+
+Articles = Articles()
+
+@app.route('/')
+def index():
+       return "index"
+
+@app.route('/articles')
+def articles():
+       return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+       return render_template('article.html', id = id)
+
+
+
+
+
+
+if __name__== '_main_':
+    app.run()
---
+++ data.py
@@ -0,0 +1,29 @@
+def Articles():
+
+    articles = [
+        {
+            'id':'1',
+            'title':'Article One',
+            'body':'This is article one',
+            'author':'Lilian Kendi',
+            'create_date':'23-09-2018'
+
+        },
+        {
+            'id':'2',
+            'title':'Article Two',
+            'body':'This is article Two',
+            'author':'Kevin Kemboi',
+            'create_date':'22-09-2018'
+
+        },
+        {
+            'id':'3',
+            'title':'Article Three',
+            'body':'This is article Three',
+            'author':'John Doe',
+            'create_date':'21-09-2018'
+
+        },
+    ]
+    return articles
