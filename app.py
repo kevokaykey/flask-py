c6baf7b8412a2d75ed83d0beca961b72f72d77df
kevin <kkemboi08@gmail.com>
2018-09-28 18:45:33 +0300

    second commit

--- app.py
+++ app.py
@@ -13,16 +13,16 @@ def index():
 
 @app.route('/articles')
 def articles():
-       return render_template('articles.html', articles = Articles)
+       return render_template('articles.html', articles = Articles )
 
 @app.route('/article/<string:id>/')
 def article(id):
-       return render_template('article.html', id = id)
+       return render_template('article.html', id = id, articles = Articles)
 
 
 
 
 
 
-if __name__== '_main_':
+if __name__== '__main__':
     app.run()
