3c3379f985f3b8bc7543ec0f3c74bc1457f80465
kevin <kkemboi08@gmail.com>
2018-09-30 22:54:47 +0300

    second change

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from data import Articles
+from flaskext.mysql import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt 
 
 app = Flask(__name__)
 app.debug = True
+
+#config MysSQl
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskapp'
+app.config['MYSQL_CURSORCLASS'] = 'localhost'
 
 
 Articles = Articles()
@@ -17,12 +27,26 @@ def articles():
 
 @app.route('/article/<string:id>/')
 def article(id):
-       return render_template('article.html', id = id, articles = Articles)
+       return render_template('article.html', id = id)
 
 
+class RegisterForm(Form):
+    name = StringField('Name', [validators.length(min=1, max=50)])
+    username = StringField('Username', [validators.length(min=4, max=25)])
+    email = StringField('Email', [validators.length(min=6,max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message ="Passwords do not match")
+    ])
+    confirm = PasswordField('Confirm Password')
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
 
-
+      return render_template('register.html', form = form)
+    return render_template('register.html', form = form) 
 
 if __name__== '__main__':
     app.run()
